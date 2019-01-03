d6191fb6c4b12abd000bf11de2c2e35d838c35cb
Jakub Skoneczny <skoneczny.j@gmail.com>
2019-01-03 22:16:47 +0100

    added pagination and sorting for getting all movies

--- controllers/movies.js
+++ controllers/movies.js
@@ -3,8 +3,14 @@ const Movie = require("../models/movie");
 
 // Get all movies
 module.exports.getAllMovies = async function (req, res, next) {
+    // For pagination
+    const {perPage, offset} = req.query;
+
+    // For sorting
+    const {sortBy, order} = req.query;
+
     try {
-        const movies = await Movie.getAllMovies();
+        const movies = await Movie.getAllMovies(perPage, offset, sortBy, order);
         res.status(200).json(movies);
     } catch (e) {
         return next(e);
@@ -12,10 +18,15 @@ module.exports.getAllMovies = async function (req, res, next) {
 };
 
 // Get one movie
-module.exports.getOneMovie = async function (req, res) {
+module.exports.getOneMovie = async function (req, res, next) {
     try {
         const movie = await Movie.getOneMovie(req.params.id);
-        res.status(200).json(movie);
+
+        if(!movie.length)
+            return next();
+        else
+            res.status(200).json(movie);
+
     } catch (e) {
         return next(e);
     }
--- models/movie.js
+++ models/movie.js
@@ -1,9 +1,25 @@
 // Database import
 const DB = require("./index");
 
-module.exports.getAllMovies = async function () {
+module.exports.getAllMovies = async function (perPage = 10, offset = 0, sortBy = 'id', order = 'desc') {
+    const sortable = ["id", "title", "year", "length", "rate"];
+
+    let sortQuery = '',
+        orderQuery = '';
+
+    sortBy = sortBy.toLowerCase();
+
+    if (sortable.includes(sortBy))
+        sortQuery = `ORDER BY ${sortBy}`;
+
+    order = order.toUpperCase();
+
+    if (order === "ASC" || order === "DESC")
+        orderQuery = `${order}`;
+
     try {
-        return await DB.query("SELECT * FROM movies");
+        console.log(`SELECT * FROM movies ${sortQuery} ${orderQuery} LIMIT ${perPage} OFFSET ${offset}`);
+        return await DB.query(`SELECT * FROM movies WHERE language='Polish' ${sortQuery} ${orderQuery} LIMIT ${perPage} OFFSET ${offset}`);
     } catch (err) {
         throw new Error("Error while selecting all movies.");
     }
