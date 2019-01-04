a8f70b90a8d053e16c2683514cc16898a0ae8073
Jakub Skoneczny <skoneczny.j@gmail.com>
2019-01-04 09:39:53 +0100

    added validation for pagination

--- models/movie.js
+++ models/movie.js
@@ -2,6 +2,12 @@
 const DB = require("./index");
 
 module.exports.getAllMovies = async function (perPage = 10, offset = 0, sortBy = 'id', order = 'desc') {
+    if(Number.isNaN(parseInt(perPage)))
+        perPage = 10;
+
+    if(Number.isNaN(parseInt(offset)))
+        offset = 0;
+
     const sortable = ["id", "title", "year", "length", "rate"];
 
     let sortQuery = '',
@@ -19,7 +25,7 @@ module.exports.getAllMovies = async function (perPage = 10, offset = 0, sortBy =
 
     try {
         console.log(`SELECT * FROM movies ${sortQuery} ${orderQuery} LIMIT ${perPage} OFFSET ${offset}`);
-        return await DB.query(`SELECT * FROM movies WHERE language='Polish' ${sortQuery} ${orderQuery} LIMIT ${perPage} OFFSET ${offset}`);
+        return await DB.query(`SELECT * FROM movies ${sortQuery} ${orderQuery} LIMIT ${perPage} OFFSET ${offset}`,);
     } catch (err) {
         throw new Error("Error while selecting all movies.");
     }
