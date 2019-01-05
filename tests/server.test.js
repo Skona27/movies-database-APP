1a8232ccec088a78cd836f5228d71b01443d6187
Jakub Skoneczny <skoneczny.j@gmail.com>
2019-01-05 12:07:29 +0100

    added tests for server

---
+++ tests/server.test.js
@@ -0,0 +1,24 @@
+// Requirements
+const request = require("supertest");
+const expect = require("expect");
+
+const app = require("../app");
+
+describe("Server", () => {
+    it("should respond", done => {
+        request(app)
+            .get("/movies")
+            .expect(200)
+            .end(done);
+    });
+
+    it("should handle error", done => {
+        request(app)
+            .get("/random-route")
+            .expect(404)
+            .expect((req) => {
+                expect(req.body).hasOwnProperty("error")
+            })
+            .end(done);
+    });
+});
