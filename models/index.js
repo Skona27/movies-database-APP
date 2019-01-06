9ad3a0bad429aa10fb80d515882aecf81f16e06f
Jakub Skoneczny <skoneczny.j@gmail.com>
2019-01-06 13:16:07 +0100

    re-added models/index.js file because of vulnerable info

---
+++ models/index.js
@@ -0,0 +1,35 @@
+// Requirements
+require("dotenv").load();
+const mysql = require('mysql');
+const util = require('util');
+
+let DB = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_DATABASE,
+    connectionLimit: 10
+});
+
+DB.getConnection((err, connection) => {
+    if (err) {
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+            console.error('Database connection was closed.')
+        }
+        if (err.code === 'ER_CON_COUNT_ERROR') {
+            console.error('Database has too many connections.')
+        }
+        if (err.code === 'ECONNREFUSED') {
+            console.error('Database connection was refused.')
+        }
+
+        console.log(err);
+    }
+
+    if (connection)
+        connection.release();
+});
+
+DB.query = util.promisify(DB.query);
+
+module.exports = DB;
