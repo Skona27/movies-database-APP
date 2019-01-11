e541391a61b89041b4d8aa883c0b7e928e1a7fe7
Jakub Skoneczny <skoneczny.j@gmail.com>
2019-01-11 15:37:00 +0100

    added pagination and searching

--- client/src/components/SearchBar/SearchBar.js
+++ client/src/components/SearchBar/SearchBar.js
@@ -1,14 +1,26 @@
-import React from "react";
+import React, {Component} from "react";
 
-const SearchBar = props => (
-  <form className="input-group mb-3 searchBar">
-    <input type="text" className="form-control" placeholder="Search a movie" aria-label="Search a movie" />
-    <div className="input-group-append">
-      <button className="btn btn-primary" type="button">
-        <i class="fas fa-search"></i>
-      </button>
-    </div>
-  </form>
-)
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      search: ""
+    }
+  }
+
+  render() {
+    return (
+      <form className="input-group mb-3 searchBar" onSubmit={(e) => {e.preventDefault(); this.props.search(this.state.search)}}>
+        <input type="text" className="form-control" placeholder="Search a movie"
+         onChange={(e) => {this.setState({search: e.target.value})}} value={this.state.search}/>
+        <div className="input-group-append">
+          <button className="btn btn-primary" type="submit">
+            <i class="fas fa-search"></i>
+          </button>
+        </div>
+      </form>
+    )}
+}
 
 export default SearchBar;
--- client/src/containers/MainPage/MainPage.js
+++ client/src/containers/MainPage/MainPage.js
@@ -12,27 +12,67 @@ class MainPage extends Component {
 
     this.state = {
       movies: null,
-      links: null
+      links: null,
+      jwt: null
     };
 
     this.changePage = this.changePage.bind(this);
+    this.searchMovie = this.searchMovie.bind(this);
   }
 
   async changePage(href) {
-    
     try {
-      let result = await axios.get(href);
-      console.log(result);
+      let result;
+
+      if(this.state.jwt) {
+        result = await axios.get(href, { headers: {"Authorization" : `Bearer ${this.state.jwt}`} });
+      } else {
+        result = await axios.get(href);
+      }
       this.setState({movies: result.data.data, links: result.data.links});
     } catch(err) {
       console.log(err);
     }
   }
 
+  async searchMovie(movie) {
+    console.log("SEARCHING MOVIE");
+    try {
+      let result;
+      let href = `http://localhost:3001/api/movies?search=${movie}&perPage=4`
+
+      if(this.state.jwt) {
+        result = await axios.get(href, { headers: {"Authorization" : `Bearer ${this.state.jwt}`} });
+      } else {
+        result = await axios.get(href);
+      }
+      this.setState({movies: result.data.data, links: result.data.links});
+    } catch(err) {
+      console.log(err);
+    }
+  }
+
+  loadButton() {
+    let links = this.state.links;
+    if (links) {
+      const createBtn = links.filter(link => link.rel === "create");
+
+      if(createBtn.length) {
+        return <Button value="Add new movie" style="primary" />
+      }
+    }
+  }
+
   async componentDidMount() {
     try {
-      let result = await axios.get("http://localhost:3001/api/movies?perPage=4");
-      this.setState({movies: result.data.data, links: result.data.links});
+      let result;
+      
+      if(this.state.jwt) {
+        result = await axios.get("http://localhost:3001/api/movies?perPage=4", { headers: {"Authorization" : `Bearer ${this.state.jwt}`} });
+      } else {
+        result = await axios.get("http://localhost:3001/api/movies?perPage=4");
+      }
+        this.setState({movies: result.data.data, links: result.data.links});
     } catch(err) {
       console.log(err);
     }
@@ -41,9 +81,9 @@ class MainPage extends Component {
   render() {
     return (
       <div className="mainPage">
-        <SearchBar />
+        <SearchBar search={this.searchMovie} />
         <MovieList movies={this.state.movies} links={this.state.links} changePage={this.changePage}/>
-        <Button value="Add new movie" style="primary" />
+        {this.loadButton()}
       </div>
     )
   }
