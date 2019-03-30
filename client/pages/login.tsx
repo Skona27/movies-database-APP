346903e93a2716cae70025a1916ce7b1e0ba19f8
Jakub Skoneczny <skoneczny.j@gmail.com>
2019-03-30 20:27:27 +0100

    Add indication for navbar active element, Change styles for movieCard component

---
+++ client/pages/login.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Navbar } from "../components/Navbar";
+import { Wrapper } from "../components/Wrapper";
+
+function Login() {
+  return (
+    <>
+      <Navbar />
+      <Wrapper padding="10rem 0" backgroundColor="green">
+        Login Page
+      </Wrapper>
+    </>
+  );
+}
+
+export default Login;
